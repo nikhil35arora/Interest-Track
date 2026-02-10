c0c2da2c764901f5ee8ad8df4fb819010abba674
{nikhil35arora} <{nikhil.35.arora@gmail.com}>
2026-02-10 18:28:19 +0530

    updated package.json

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,9 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 // If someone goes to the main URL, show the index.html file
-app.get('/:path*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+// This version works with the newest Express rules
+app.get(/^(?!\/api).+/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
 app.use(express.json());
 require('dotenv').config(); 
