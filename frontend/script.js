19625ee09e98fb592c774e12a3eda5dd35de2325
{nikhil35arora} <{nikhil.35.arora@gmail.com}>
2026-02-10 19:43:59 +0530

    updated script

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,6 @@
-
+const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://localhost:8000' 
+    : 'https://interest-track.onrender.com';
 // 1. Redirect if not logged in
 const userId = localStorage.getItem('loggedUserId');
 if (!userId) {
